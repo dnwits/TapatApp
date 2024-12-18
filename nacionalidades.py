e4c614c707cd0c34af822a30074c59daf80fb895
Valery Michelle Escobar <v.m.e.c1122002@gmail.com>
2024-12-18 12:58:12 +0100

    aaa

--- nacionalidades.py
+++
@@ -1,8 +0,0 @@
-import requests
-# https://nationalize.io/documentation
-url = "https://api.nationalize.io/?name=nathaniel"
-response = requests.get(url)
-if response.status_code == 200:
-    print("Contingut:", response.json())
-else:
-    print("Error:", response.status_code)
