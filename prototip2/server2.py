45f14f275571e0cce09058c059ad618f7664dad1
Valery Michelle Escobar <v.m.e.c1122002@gmail.com>
2025-02-18 14:51:28 +0100

    comprovar client

--- prototip2/server2.py
+++ prototip2/server2.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import dadesServer as dades
-from dadesServer import User, Child, users, children, relation_user_child
+from dadesServer import User, Child, users, children, relation_user_child, treatments
 
 # Exemple d'ús de la llista d'usuaris
 for x in dades.users:
@@ -21,14 +21,11 @@ class DAOUsers:
                 return u #.__dict__
         return None
     
-# class DAOChild(id_user):
-#     def __init__(self):
-#         self.id=dades.users
-#     def getChildByUserId(self,id):
 class DAOChild:
     def __init__(self):
         self.children = children
         self.relations = relation_user_child
+        self.treatments = treatments
 
     def getChildrenByUserId(self, user_id):
         result = []
@@ -36,8 +33,23 @@ class DAOChild:
             if relation["user_id"] == user_id:
                 for child in self.children:
                     if child.id == relation["child_id"]:
-                        result.append(child)
+                        # result.append(child)
+                        # Buscar el tratamiento asociado
+                        treatment = self.getTreatmentById(child.treatment_id)
+                        result.append({
+                            "id": child.id,
+                            "name": child.child_name,
+                            "sleep_average": child.sleep_average,
+                            "treatment": treatment.name if treatment else "No treatment",
+                            "time": child.time
+                        })
         return result
+    
+    def getTreatmentById(self, treatment_id):
+        for treatment in self.treatments:
+            if treatment.id == treatment_id:
+                return treatment
+        return None
 # declara que utilizaremos flask
 # variable que llama al metodo DAOUsers de antes
 app = Flask(__name__)
@@ -69,17 +81,27 @@ def get_children(username):
 
     children = daoChild.getChildrenByUserId(user.id)
     if children:
-        return jsonify([
-            {
-                "id": child.id,
-                "name": child.child_name,
-                "sleep_average": child.sleep_average,
-                "treatment_id": child.treatment_id,
-                "time": child.time
-            } for child in children
-        ]), 200
+        return jsonify(children), 200
     else:
         return jsonify({"error": "Aquest usuari no té nens associats"}), 404
+# def get_children(username):
+#     user = daoUser.getUserByUsername(username)
+#     if not user:
+#         return jsonify({"error": "Usuari no trobat..."}), 404
+
+#     children = daoChild.getChildrenByUserId(user.id)
+#     if children:
+#         return jsonify([
+#             {
+#                 "id": child.id,
+#                 "name": child.child_name,
+#                 "sleep_average": child.sleep_average,
+#                 "treatment_id": child.treatment_id,
+#                 "time": child.time
+#             } for child in children
+#         ]), 200
+#     else:
+#         return jsonify({"error": "Aquest usuari no té nens associats"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True, host="192.168.144.157", port=10050)
